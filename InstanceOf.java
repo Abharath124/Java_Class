4e6b7fbeff1fdf2fc30a5497327e14df211f2403
Anumandla Bharath <bharath124anumandla@gmail.com>
2025-03-21 15:04:54 +0530

    Add files via upload

---
+++ InstanceOf.java
@@ -0,0 +1,18 @@
+package instanceOf;
+class Animal{
+	
+}
+class Dog{
+	
+}
+public class InstanceOf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Animal a = new Animal();
+        System.out.println(a instanceof Animal);
+        Dog d = new Dog();
+        System.out.println(d instanceof Dog);
+	}
+
+}
